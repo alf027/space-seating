e1b01020b5a8888d0768f4f7ba708772aa82e266
John Alfano <alf027@gmail.com>
2015-07-10 11:40:03 -0600

    routes, html, other stuff

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 var http = require('http'),
   router = require('./router.js'),
   url = require('url');
+var NodeSession = require('node-session'),
+  session = new NodeSession({secret: 'Q3UBzdH9GEfiRCTKbi5MTPyChpzXLsTD'})
 
 var server = http.createServer(function (req, res) {
   if (req.url === '/favicon.ico') {
@@ -11,14 +13,16 @@ var server = http.createServer(function (req, res) {
     res.end();
     return
   }
-  var path = url.parse(req.url).pathname;
-  var currentRoute = router.match(path);
-  if(!currentRoute){
-    res.writeHead(404, {'Content-Type':'text/html'});
-    res.end('404');
-  } else {
-    currentRoute.fn(req, res, currentRoute);
-  }
+  session.startSession(req, res, function () {
+    var path = url.parse(req.url).pathname;
+    var currentRoute = router.match(path);
+    if (!currentRoute) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.end('404');
+    } else {
+      currentRoute.fn(req, res, currentRoute);
+    }
+  });
 
 });
 
@@ -26,3 +30,5 @@ server.listen(8080, function (err) {
   if (err) console.log('Doah', err);
   console.log('Woot. A server is running on port 8080')
 });
+
+
--- router.js
+++ router.js
@@ -3,20 +3,103 @@
  */
 var routes = require('routes')(),
   fs = require('fs'),
-
+  db = require('monk')('localhost/spaceSeats'),
+  users = db.get('users'),
+  seats = db.get('seats'),
   qs = require('qs'),
   mime = require('mime'),
-  view = require('./view');
+  view = require('./view'),
+  bcrypt = require('bcrypt');
 
-routes.addRoute('/', function(req,res,url) {
+routes.addRoute('/', function (req, res, url) {
 
-  if(req.method==='GET') {
-        template = view.render('/index',{});
-        res.write(template);
-        res.end()
+  if(req.session.get('email')) {
+    console.log(req.session.get('email'));
+    seats.find({},function(err,docs) {
+      //if(err) {
+      //  res.end('404');
+      //}
+      var template = view.render('/index',{seats:docs});
+      res.end(template);
+    });
 
-      }
-
+  } else {
+    req.session.flush();
+    res.writeHead(302, {'Location': '/login'});
+    res.end()
+  }
 
 });
+
+
+routes.addRoute('/register', function (req, res, url) {
+  if (req.method === 'GET') {
+    // 1. Respond with the `sessions/register`page with
+    //    our view module. `view.render('path', {})`
+    res.setHeader('Content-Type', 'text/html');
+    var template = view.render('/register', {title: 'Register'});
+    res.end(template)
+  }
+
+  if (req.method === 'POST') {
+    // 1. Collect form data with async accumulator
+    // 2. Insert user into database
+    // 3. Create the session with req.session.put('email', doc.email)
+    //    Note: you will need to do this inside of the monk
+    //    callback function.
+    var data = '';
+
+    req.on('data', function (chunk) {
+      data += chunk
+    });
+
+    req.on('end', function () {
+      var user = qs.parse(data);
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+      users.insert(user, function (err, doc) {
+        if (err) {
+          res.writeHead(302, {'Location': '/'});
+          res.end();
+          return
+        }
+        req.session.put('email', doc.email);
+        res.writeHead(302, {'Location': '/'});
+        res.end()
+      })
+    })
+  }
+});
+
+
+routes.addRoute('/login', function (req, res, url) {
+  if (req.method === 'GET') {
+    var template = view.render('/login', {title: 'Log In'});
+    res.end(template)
+
+  }
+  if (req.method === 'POST') {
+    var data = '';
+
+    req.on('data', function (chunk) {
+      data += chunk
+    });
+
+    req.on('end', function () {
+      var user = qs.parse(data);
+      users.findOne({email: user.email}, function (err, doc) {
+        if (doc) {
+          if (bcrypt.compareSync(user.password, doc.password)) {
+            req.session.put('email', doc.email);
+            res.writeHead(302, {'Location': '/'});
+            res.end();
+          } else {
+            res.writeHead(302, {'Location': '/register'});
+            res.end();
+          }
+        }
+      });
+    });
+  }
+});
+
 module.exports = routes;
